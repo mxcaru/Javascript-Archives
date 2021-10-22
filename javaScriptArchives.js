98b1b7ef098f7d40bb822e35d8406642b0201c59
Mark <markjosephcarungay@gmail.com>
2021-10-22 12:28:11 +0800

    return missing element

--- javaScriptArchives.js
+++ javaScriptArchives.js
@@ -145,3 +145,27 @@ function binarySearch(sortedArray, value) {
   }
   return `no value found`;
 }
+
+//NOTE: Return the Missing Element
+// Fellow code warrior, we need your help! We seem to have lost one of our sequence elements, and we need your help to retrieve it!
+
+// Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
+
+// Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
+
+// Examples:
+// [0, 5, 1, 3, 2, 9, 7, 6, 4] --> 8
+// [9, 2, 4, 5, 7, 0, 8, 6, 1] --> 3
+
+//Loop solution
+function getMissingElement(superImportantArray) {
+  let sum = 0;
+  for (let i = 0; i < superImportantArray.length; i++) {
+    sum -= superImportantArray[i];
+  }
+  return 45 - -sum;
+}
+
+//reduce method
+const getMissingElement = (superImportantArray) =>
+  superImportantArray.reduce((sum, elem) => sum - elem, 45);
